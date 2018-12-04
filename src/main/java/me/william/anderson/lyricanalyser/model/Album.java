fbfe19a8a8cd71055dedcdd3e2bb155b45cc9c85
William Anderson <carrig212@gmail.com>
2018-12-04 14:57:03 +0000

    Fix database enitity relationships

--- src/main/java/me/william/anderson/lyricanalyser/model/Album.java
+++ src/main/java/me/william/anderson/lyricanalyser/model/Album.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.Date;
 import java.util.Set;
@@ -16,6 +17,7 @@ public class Album extends Music {
     @OneToMany(mappedBy = "album")
     private Set<Track> tracks;
 
+    @ManyToOne
     @JoinColumn(name = "artistId")
     private Artist artist;
 
--- src/main/java/me/william/anderson/lyricanalyser/model/Track.java
+++ src/main/java/me/william/anderson/lyricanalyser/model/Track.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.Duration;
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 @NoArgsConstructor // For JPA purposes
 @Entity
 public class Track extends Music {
+    @ManyToOne
     @JoinColumn(name = "albumId")
     private Album album;
 
