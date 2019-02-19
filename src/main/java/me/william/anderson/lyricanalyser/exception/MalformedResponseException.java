2a39b8485c561b2752ea64458f18f166e36eb62c
William Anderson <carrig212@gmail.com>
2019-02-19 14:35:37 +0000

    Remove unnecessary exception classes

--- src/main/java/me/william/anderson/lyricanalyser/exception/MalformedResponseException.java
+++
@@ -1,7 +0,0 @@
-package me.william.anderson.lyricanalyser.exception;
-
-public class MalformedResponseException extends Exception {
-    public MalformedResponseException(String url, Exception cause) {
-        super("The server at " + url + " returned a malformed HTTP response", cause);
-    }
-}
--- src/main/java/me/william/anderson/lyricanalyser/exception/StatusCodeException.java
+++ src/main/java/me/william/anderson/lyricanalyser/exception/StatusCodeException.java
@@ -1,11 +1,7 @@
 package me.william.anderson.lyricanalyser.exception;
 
 public class StatusCodeException extends Exception {
-    public StatusCodeException(int expected, Exception cause) {
-        super("Expected status code " + expected, cause);
-    }
-
-    public StatusCodeException(int expected, int actual) {
-        super("Expected status code " + expected + " but found " + actual);
+    public StatusCodeException(int expected, int actual, String url) {
+        super(url + " responded with status code " + actual + " instead of the expected " + expected);
     }
 }
