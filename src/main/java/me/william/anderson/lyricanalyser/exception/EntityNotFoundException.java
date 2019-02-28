5bc845d1e021b5299594826778dc12b19527b8f1
William Anderson <carrig212@gmail.com>
2019-02-28 10:45:55 +0000

    Remove unused Exception class

--- src/main/java/me/william/anderson/lyricanalyser/exception/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package me.william.anderson.lyricanalyser.exception;
-
-import me.william.anderson.lyricanalyser.model.Music;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(long id, Class<? extends Music> type) {
-        super("No " + type.getSimpleName() + " with ID " + id + " could be found");
-    }
-}
